97745034aceb8822e778fd4e4d57641e55dd1017
Aayush Arora <aayusharora2510@gmail.com>
2025-06-23 11:52:36 +0530

    Update GetStarted.jsx

--- src/components/GetStarted/GetStarted.jsx
+++ src/components/GetStarted/GetStarted.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import "./GetStarted.css";
 
 const GetStarted = () => {
-    // Pre-written text for the email body
-    const preWrittenText = encodeURIComponent("Hello, Please follow the format asked here without these your request will be discarded \n\nInterested in hiring you for a project or Internship or Job ?. \n\n Mention the name of your company. \n\n Please provide atleast 2 sources of contact. \n\nThank You");
+    const preWrittenText = encodeURIComponent(
+        `Hello,\n\nInterested in hiring/collaborating with you.\n\nPlease provide the following details:\n\n- Your Name:\n- Company Name:\n- Contact Information (at least 2 sources):\n- Additional Notes:\n\nThank you!`
+    );
 
     return (
         <div id="get-started" className="g-wrapper">
@@ -11,12 +12,16 @@ const GetStarted = () => {
                 <div className="flexColCenter inner-container">
                     <span className="primaryText">Contact</span>
                     <span className="secondaryText">
-                        Contact for hiring or if you want to collaborate for any project
+                        Contact for hiring or project collaboration.
                         <br />
-                        Kindly mention details in the mail.
+                        Kindly include all the requested details in your message.
                     </span>
-                    {/* Modified button with mailto link including pre-written text */}
-                    <a href={`mailto:aayusharora2510@gmail.com?subject=Hiring Inquiry&body=${preWrittenText}`} className="button">Contact Now</a>
+                    <a 
+                        href={`mailto:aayusharora2510@gmail.com?subject=Hiring Inquiry&body=${preWrittenText}`} 
+                        className="button"
+                    >
+                        Contact Now
+                    </a>
                 </div>
             </div>
         </div>
